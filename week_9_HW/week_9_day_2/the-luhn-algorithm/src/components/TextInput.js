e1443249c654cc19cfced00b4a42492350ce2588
qianliu <liu.qiannfu@gmail.com>
2023-07-21 23:45:42 -0400

    remove format

--- week_9_HW/week_9_day_2/the-luhn-algorithm/src/components/TextInput.js
+++ week_9_HW/week_9_day_2/the-luhn-algorithm/src/components/TextInput.js
@@ -3,21 +3,10 @@ import validCard from './ValidCard';
 function TextInput() {
   const [inputValue, setInputValue] = useState('');
 
-  const formatCardNumber = (value) => {
-    // Remove any non-numeric characters from the input
-    const numericValue = value.replace(/\D/g, '');
-
-    // Split the numericValue into groups of four digits
-    const formattedValue = numericValue.replace(/(\d{4})(?=\d)/g, '$1 ');
-
-    // Limit the formattedValue to 19 characters (16 digits + 3 spaces)
-    return formattedValue.slice(0, 19);
-  };
-
 
   const handleChange = (event) => {
     // handle changes from input info and update the value of {state}
-    setInputValue(formatCardNumber(event.target.value));
+    setInputValue((event.target.value));
   };
 
   const handleSubmit = () => {
