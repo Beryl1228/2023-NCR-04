22a8d2ecd6bc0f2b5bcecf92137d92730027db05
qianliu <liu.qiannfu@gmail.com>
2023-08-14 11:32:02 -0400

    Tip Calculator express application created.

--- week_12/week_12_day_2_Lab/express-greedings/server.js
+++ week_12/week_12_day_2_Lab/express-greedings/server.js
@@ -6,6 +6,9 @@ const app = express();
 
 const port = 3001;
 
+
+/*
+//Greetings 
 // Route for '/greeting' without any parameters
 app.get('/greeting', (req, res) => {
     console.log(req.query); // Print query parameters
@@ -17,6 +20,15 @@ app.get('/greeting/:name', (req, res) => {
     console.log(req.params); // Print route parameters
     res.send(`Hi ${req.params.name}! It's so great to see you!`);
 });
+*/
+
+ //Tip Calculator
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    console.log(req.query); // Print query parameters
+    console.log(req.params)
+    res.send(`<h1>Tip Total: ${req.params.tipPercentage} </h1>`);
+}); 
+
 
 app.listen(port, () => {
     console.log('listening on port', port);
