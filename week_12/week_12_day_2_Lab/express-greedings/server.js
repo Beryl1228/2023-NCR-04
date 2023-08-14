55ee8cb88578095a83699a64e149340bf3abba54
qianliu <liu.qiannfu@gmail.com>
2023-08-14 11:32:47 -0400

    Magic 8 Ball express application created.

--- week_12/week_12_day_2_Lab/express-greedings/server.js
+++ week_12/week_12_day_2_Lab/express-greedings/server.js
@@ -22,13 +22,32 @@ app.get('/greeting/:name', (req, res) => {
 });
 */
 
- //Tip Calculator
+/*  //Tip Calculator
 app.get('/tip/:total/:tipPercentage', (req, res) => {
     console.log(req.query); // Print query parameters
     console.log(req.params)
     res.send(`<h1>Tip Total: ${req.params.tipPercentage} </h1>`);
 }); 
+ */
 
+
+//Magic 8 Ball
+const magicBalls = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+
+let ball = magicBalls[Math.floor(Math.random()*magicBalls.length)];
+
+app.get('/', (req, res) => {
+    console.log(req.query); // Print query parameters
+    console.log(req.params)
+    res.send(`<h1> magic 8 ball </h1>
+    <a href="http://localhost:3001/magic/Will%20I%20Be%20A%20Millionaire">Will I be a Millionaire ?</a>`);
+}); 
+
+app.get('/magic/Will%20I%20Be%20A%20Millionaire', (req, res) => {
+    console.log(req.query); // Print query parameters
+    console.log(req.params)
+    res.send(`<h1> ${ball}</h1>`);
+});
 
 app.listen(port, () => {
     console.log('listening on port', port);
