dbcfd019f6eaa81fc87ed523b5827adc640a5a9b
qianliu <liu.qiannfu@gmail.com>
2023-08-18 22:55:07 -0400

    Connected my database, can see json in the browser

--- week_13/wk_13_d_2_HW/pokemon_app/server.js
+++ week_13/wk_13_d_2_HW/pokemon_app/server.js
@@ -4,11 +4,19 @@ const express = require('express');
 //set express()to a variable
 const app = express();
 
+const pokemon = require('./models/pokemon')
+
 app.get('/',(req,res)=>{
     res.send(console.log('Welcome to the Pokemon App!'))
+})
+
+
+app.get('/pokemon',(req,res)=>{
+    res.send(pokemon)
 })
 
 //set a variable of portto 3000
 app.listen(3000,() => {
     console.log('listening');
 });
+
